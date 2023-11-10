9b72199aad5533fba0492300d35ca1d95c72dc1a
UtescherIntrieri <18ianaui1999@gmail.com>
2023-11-10 12:03:59 -0300

    Task - Listagem por pousadas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @inns = Inn.active
+    @last_3_inns = Inn.last(3).reverse
+    @inns = Inn.order('brand_name').first(Inn.all.length - @last_3_inns.length)
     @rooms = Room.vacant
   end
 end
